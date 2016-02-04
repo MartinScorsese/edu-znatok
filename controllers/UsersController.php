5889d4f4b1219703edb7d7956b9ecee8338c4f0b
AK-VoronM <ak.alz@mail.ru>
2016-02-04 19:38:22 +0300

    Users заготовка
    Заготовка контроллера регистрации, и контроллера пользователя.

--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -1,16 +1,42 @@
 <?php
-class CoursesController 
+class UsersController 
 {
-    public function actionShow(){ 
+    
+    public function actionShowUsers(){     
+        
+        $auth = Auth::checkAuth();
         
         $view = new View();
-
-      //  $view->user_login = $user->user_login;
-       // $view->user_group = $user->user_group;
-
-        $view->display('header.php');
-        $view->display('content.php');
-        $view->display('footer.php');
-
+        $view->auth = $auth;
+        if ($auth) {
+            $user = Auth::getUser();
+            if ($user->user_group == 1){
+                $view->user_login = $user->user_login;
+                $view->user_group = $user->user_group;
+                
+                $view->users = Users::getUsers();
+                $view->display('header.php');
+                $view->display('users/user_list.php');
+            }
+            $view->display('footer.php');
+        }else{
+            header("Location: /learns/");
+        }
     }
+        
+    
+    
+    public function actionAddUser(){ 
+         
+        //Добавляет нового пользователя в базу
+        
+    }
+    
+       
+    public function actionDeleteUser(){     
+        
+        //Удаляет пользователя из базы
+        
+    }
+    
 }
--- models/Users.php
+++ models/Users.php
@@ -1,15 +1,14 @@
 <?php
 class Users extends AbstractModel {
+    
     public $date;
     
-    public function getDate(){
+    public function getUsers(){
         
-       // $db = new DB;
-       // $query = "SELECT user_birthday FROM death_timer WHERE user_id='1'";
-       // $date = $db->query($query);
+        $db = new DB;
+        $query = "SELECT user_login, user_id, user_group FROM users";
+        $date = $db->query($query);
         
-		$date[0] = time();
-        return $date[0]->user_birthday;       
-    }
-    
-} 
+        return $date;
+    }    
+}
